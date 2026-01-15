48bc0be7e02cee87c0a773bf3b8f0a627115c6e0
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-01-15 11:29:18 +0200

    tehtävä 1

---
+++ kokeilu+muistio.py
@@ -0,0 +1,32 @@
+'''
+print ("Hello World")
+print ("Hei,sanoi \nVille")
+print ("Hyvää")
+print ("huomenta")
+käyttäjä = input("Anna nimesi: ")
+ikä=input ("Anna ikäsi:")
+print(f"Hei, sanoi:\n\t{käyttäjä} ikä:{ikä}")
+'''
+
+# luku1 = float(input("Anna luku:"))
+# luku2 = float(input("Anna luku:"))
+# tulo = luku1 * luku2
+# print(f"lukujen {luku1} ja {luku2} tulo on {tulo}")
+
+# pisteet = 50  # Muuttujan pisteet arvo on nyt 50
+# print(pisteet)  # Tulostaa: 50
+#
+# pisteet = 120  # Nyt muuttujan pisteet arvo on 120
+# print(pisteet)  # Tulostaa: 120
+
+# muuttuja = "tekstiä" #merkkijono eli string
+# print(muuttuja)
+# luku = 123 #kokonaisluku eli int
+# print(luku)
+# liukuluku= 3.2 #liukuluku eli float
+# print (liukuluku)
+# totuusarvo = true totuusarvo eli boolean
+# print (totuusarvo)
+
+
+
