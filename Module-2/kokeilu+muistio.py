e36ad3c7f36d2231ffce4d8e55ed9da321caf73c
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-01-22 11:32:22 +0200

    Add module3 assignment2

--- Module-2/kokeilu+muistio.py
+++ Module-2/kokeilu+muistio.py
@@ -42,5 +42,40 @@ print(f"Hei, sanoi:\n\t{käyttäjä} ikä:{ikä}")
 
 #print("3-digit code:", three_digits)
 
-
-
+# komento = input("Anna komento: ")
+# while komento != "lopeta" and komento != "Lopeta":
+#     print("Suoritan toiminnon: " + komento)
+#     komento = input("Anna komento: ")
+# print("Toiminnot lopetettu.")
+# import random
+#
+# noppa1 = noppa2 = heitot = 0 #nollataan kaikki 3 muuttujaa
+# while (noppa1 != 6 or noppa2 != 6):
+#         noppa1 = random.randint(1, 6)
+#         noppa2 = random.randint(1, 6)
+#         heitot += 1
+#         print(f"Heitit nopat {noppa1}, {noppa2}")
+# print(f"Tarvittiin {heitot} heittoa.")
+# eka = 1
+# while eka <= 5:
+#     toka = 1
+#     while toka <=5:
+#         print(f"{eka} kertaa {toka} on {eka*toka}")
+#         toka = toka + 1
+#     eka = eka + 1
+#  import random
+#  toistot = 0
+#  heitot_yhteensä = 0
+#  while toistot < 100000:
+#
+#      noppa1 = noppa2 = heitot = 0
+#      while (noppa1!=6 or noppa2!=6):
+#          noppa1 = random.randint(1,6)
+#         noppa2 = random.randint(1,6)
+#         heitot = heitot + 1
+#     print(f"Tarvittiin {heitot} heittoa.")
+#     toistot = toistot + 1
+#     heitot_yhteensä = heitot_yhteensä + heitot
+#
+# heitot_keskimäärin = heitot_yhteensä/toistot
+# print(f"Heitot keskimäärin: {heitot_keskimäärin:6.2f}")
---
+++ Module-3/assignment2.py
@@ -0,0 +1,17 @@
+cabin_classes= {
+    "LUX": "Upper-deck cabin with a balcony.",
+    "A": "Above the car deck, equipped with a window.",
+    "B": "Windowless cabin above the car deck.",
+    "C": "Windowless cabin below the car deck.",
+}
+cabin_class = input("Enter the cabin class (LUX, A, B, or C): ")
+if cabin_class== "LUX":
+    print(cabin_classes[cabin_class])
+elif cabin_class== "A":
+    print(cabin_classes[cabin_class])
+elif cabin_class== "B":
+    print(cabin_classes[cabin_class])
+elif cabin_class== "C":
+    print(cabin_classes[cabin_class])
+else:
+    print("Invalid cabin class.")
