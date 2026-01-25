fb7e3e84b481e0554ef10d9b2f8b42e9010581a7
HanskiZensei <hannu.vainio2@metropolia.fi>
2026-01-25 22:06:43 +0200

    Add module 4 assignment 4 & 5

---
+++ Module-4/assignment4.py
@@ -0,0 +1,12 @@
+import random
+
+random_number = random.randint (1, 10)
+user_guess = int(input("Guess a number between 1 and 10: "))
+
+while user_guess != random_number:
+    if user_guess > random_number:
+        print("Too high")
+    else:
+        print("Too low")
+    user_guess = int(input("Guess a number between 1 and 10: "))
+print("Correct")
