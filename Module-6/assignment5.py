d20422673e1cba1028cf5027614848f4f5a6afe8
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-01-30 12:27:52 +0200

    Add module 6 assignment5

---
+++ Module-6/assignment5.py
@@ -0,0 +1,5 @@
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+filtered_list = filter_even_numbers(original_list)
+print("Original list:", original_list)
+print("List with even numbers only:", filtered_list)
