98236a3d176f74f021f2dd8a9aea9c7468c6323d
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-01-30 10:30:41 +0200

    Add module 6 assignment3

--- Module-6/assignment3.py
+++ Module-6/assignment3.py
@@ -1,7 +1,11 @@
-gallon_count = int(print("Enter the volume in gallons. "))
-liters = float(gallon_count) * 3.785
+def gallons_to_liters(gallon_count):
+    liters = gallon_count * 3.785
+    print(f"{gallon_count} American gallons is {liters:.2f} liters. ")
 
 
-def gallon_liters():
-    liters = float(gallon_count) * 3.785
-    print(liters)
+while True:
+    gallon_count = float(input("Enter a volume in American gallons (negative value to quit): "))
+    if gallon_count < 0:
+        print("Program finished. ")
+        break
+    gallons_to_liters(gallon_count)
