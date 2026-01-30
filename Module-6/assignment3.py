7cefac883bc60d886569835161830b75bb8a55aa
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-01-30 09:34:46 +0200

    Add module 6 assignment3

---
+++ Module-6/assignment3.py
@@ -0,0 +1,7 @@
+gallon_count = int(print("Enter the volume in gallons. "))
+liters = float(gallon_count) * 3.785
+
+
+def gallon_liters():
+    liters = float(gallon_count) * 3.785
+    print(liters)
