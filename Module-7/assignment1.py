7fe4d8c552bd3d9b10d75983f737668fc1174d4b
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-02-02 21:32:30 +0200

    Add module 7 assignment1

--- Module-7/assignment1.py
+++ Module-7/assignment1.py
@@ -1,2 +1,10 @@
-Seasons []
-month = input("Enter a month number: ")
+Seasons {
+    "Winter":
+}
+def get_season():
+    Seasons = month
+month = input("Enter a month number (1-12): ")
+
+if month != 1-12:
+    print("Number is not inside (1-12)")
+
