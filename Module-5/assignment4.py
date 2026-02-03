c18053ca0bbd005b0e5ad50ae20aa21472d9f66a
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-02-03 18:14:42 +0200

    Add module5 assignment4

--- Module-5/assignment4.py
+++ Module-5/assignment4.py
@@ -1,8 +1,13 @@
 kaupungit = []
+
+
 for i in range(5):
     kaupunki = input("Enter the name of a city: ")
     kaupungit.append(kaupunki)
 
 
-for kaupunki in kaupungit(5):
-    print(f"The cities you entered: {kaupungit} ")
+valmislista = kaupungit
+print()
+print("\nThe cities you entered: ")
+for lista in valmislista:
+    print(lista)
