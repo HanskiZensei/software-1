168ded4ac0d49f286c8a758401b03b2a338dad89
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-02-05 15:47:15 +0200

    Add module 8

---
+++ Module-8/tietokanta.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='hade',
+    password='New_password.',
+    autocommit=True
+)
+
+
+def hae_data():
+    sql = "Select * FROM airport LIMIT 10"
+
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+
+    tulos = kursori.fetchall()
+
+    print(tulos)
+    print(tulos[0])
+    print(tulos[0][0])
+
+    for rivi in tulos:
+        print(rivi)
+
+        for alkio in rivi:
+            print("  ", alkio)
+
+
+hae_data()
