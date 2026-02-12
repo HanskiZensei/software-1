6cd6db837b1c7db9bde2222bbc97f8b6ffe24ce5
Hannu Vainio <hannu.vainio2@metropolia.fi>
2026-02-12 10:17:00 +0200

    Add module8 assignment1

--- Module-2/kokeilu+muistio.py
+++ Module-2/kokeilu+muistio.py
@@ -95,41 +95,69 @@ print(f"Hei, sanoi:\n\t{käyttäjä} ikä:{ikä}")
 
 # while roll != 6:
 #     roll = roll_dice()
-#halkasija2 = input("Enter the diameter of the second pizza (cm): ")
-#hinta2 = input("Enter the price of the second pizza (euros): ")
+# halkasija2 = input("Enter the diameter of the second pizza (cm): ")
+# hinta2 = input("Enter the price of the second pizza (euros): ")
 # Use a dictionary to store ICAO: Airport Name pairs
-airports = {}
+# airports = {}
+#
+#
+# def get_choice():
+#     print("\nAirport Data Management")
+#     print("1. Enter a new airport")
+#     print("2. Fetch airport information")
+#     print("3. Quit")
+#     choice = input("Please choose an option (1-3): ")
+#     return choice
+#
+#
+# # Main Program Loop
+# while True:
+#     user_input = get_choice()
+#
+#     if user_input == "1":
+#         icao = input("Enter the ICAO code: ").upper()
+#         name = input("Enter the airport name: ")
+#         airports[icao] = name
+#         print(f"Airport {name} with ICAO code {icao} has been added.")
+#
+#     elif user_input == "2":
+#         icao = input("Enter the ICAO code: ").upper()
+#         if icao in airports:
+#             print(f"The airport with ICAO code {icao} is {airports[icao]}.")
+#         else:
+#             print("ICAO code not found.")
+#
+#     elif user_input == "3":
+#         print("Thank you for using the Airport Data Management system. Goodbye!")
+#         break  # This exits the loop and the program
+#
+#     else:
+#         print("Invalid choice, please select 1, 2, or 3.")
 
 
-def get_choice():
-    print("\nAirport Data Management")
-    print("1. Enter a new airport")
-    print("2. Fetch airport information")
-    print("3. Quit")
-    choice = input("Please choose an option (1-3): ")
-    return choice
+def saldo_on(saldo_nyt):
+    on_matkoja = saldo_nyt >= 170
+    return on_matkoja
 
 
-# Main Program Loop
-while True:
-    user_input = get_choice()
+def matkusta(saldo_nyt, hinta):
+    saldo_nyt = saldo_nyt - hinta
+    return saldo_nyt
 
-    if user_input == "1":
-        icao = input("Enter the ICAO code: ").upper()
-        name = input("Enter the airport name: ")
-        airports[icao] = name
-        print(f"Airport {name} with ICAO code {icao} has been added.")
 
-    elif user_input == "2":
-        icao = input("Enter the ICAO code: ").upper()
-        if icao in airports:
-            print(f"The airport with ICAO code {icao} is {airports[icao]}.")
-        else:
-            print("ICAO code not found.")
+saldo_euroissa = float(input("Lataa alkusaldo: "))
+saldo = int(saldo_euroissa * 100)
 
-    elif user_input == "3":
-        print("Thank you for using the Airport Data Management system. Goodbye!")
-        break  # This exits the loop and the program
-
+while saldo_on(saldo):
+    matkatyyppi = int(input("Minkä matkan haluat leimata? 1) aikuinen 2) lapsi: "))
+    if matkatyyppi == 1:
+        matkan_hinta = 330
+    elif matkatyyppi == 2:
+        matkan_hinta = 170
     else:
-        print("Invalid choice, please select 1, 2, or 3.")
+        print("Syötä 1 tai 2 ")
+        matkan_hinta = 0
+    saldo = matkusta(saldo, matkan_hinta)
+    print(f"Matkustit ja saldoa on jäljellä {saldo/100:.2f} euroa. ")
+
+print(f"Saldosi loppui, et voi enää matkustaa. Saldoa jäi {saldo/100:.2f} euroa. ")
